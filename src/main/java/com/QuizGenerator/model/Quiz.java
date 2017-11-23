c0b5caae2d052fdd5a93e14f1cfc9ccbede738ec
mohammaduzair9 <31093445+mohammaduzair9@users.noreply.github.com>
2017-11-24 02:56:06 +0500

    Code CleanUp

--- src/main/java/com/QuizGenerator/model/Quiz.java
+++ src/main/java/com/QuizGenerator/model/Quiz.java
@@ -6,40 +6,42 @@ import java.util.List;
 
 //Quiz Class
 public class Quiz implements Serializable {
-   private String title;
-   private String desc;
-   private List<Question> questions;
+    
+    private int _id;
+    private String title;
+    private String desc;
+    private List<Question> questions;
    
-   //constructor
-   public Quiz(){
+    //constructor
+    public Quiz(){
        title=null;
        desc=null;
        questions=new ArrayList <>() ;
-   }
+    }
    
-   //setters
-   public void setTitle(String str){
+    //setters
+    public void setTitle(String str){
        title = str;
-   }
-   public void setDesc(String str){
+    }
+    public void setDesc(String str){
        desc = str;
-   }
-   public void addQues(Question quest){
+    }
+    public void addQues(Question quest){
        questions.add(quest);
-   }
-   public void setQuestList(List<Question> updList ){
+    }
+    public void setQuestList(List<Question> updList ){
        this.questions=updList;
-   }
+    }
    
-   //getters
-   public String getTitle(){
+    //getters
+    public String getTitle(){
        return title;
-   }
-   public String getDesc(){
+    }
+    public String getDesc(){
        return desc;
-   }
-   public List<Question> getQuestList(){
+    }
+    public List<Question> getQuestList(){
        return questions;
-   }
+    }
    
 }
